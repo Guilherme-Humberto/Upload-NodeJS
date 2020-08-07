8e8d80af54e5ae42121ed9a040cbab9ec104aab0
Guilherme Humberto <gbreu@outlook.com>
2020-08-07 19:23:46 -0300

    Controllers

---
+++ src/controller/controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express'
+
+class Controller {
+    public async create (req: Request, res: Response) {
+        console.log(req.file)
+    }
+}
+
+export default new Controller()
